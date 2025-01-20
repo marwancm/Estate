522453361fdf92d7f16152639935bbda54ab7285
marwancm <marawanhegazy99@gmail.com>
2025-01-20 03:57:09 +0200

    add mobile-menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navebar from './Navebar'
 
-function Header() {
+const Header = () => {
   return (
     <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full
     overflow-hidden' style={{backgroundImage: "url('/header_img.png')"}} id='Header'>
--- src/components/Navebar.jsx
+++ src/components/Navebar.jsx
@@ -1,21 +1,105 @@
-import React from 'react'
-import {assets} from '../assets/assets'
+import React, { useEffect, useState } from "react";
+import { assets } from "../assets/assets";
 
-function Navebar() {
+const Navebar = () => {
+  const [showMobilMenu, setShowMobilMenu] = useState(false);
+  useEffect(() => {
+    if (showMobilMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [showMobilMenu]);
   return (
-    <div className='absolute top-0 left-0 w-full z-10'>
-      <div className='container mx:auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
+    <div className="absolute top-0 left-0 w-full z-10">
+      <div className="container mx:auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
         <img src={assets.logo} alt="" />
-        <ul className='hidden md:flex gap-7 text-white'>
-          <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
-          <a href="#Header" className='cursor-pointer hover:text-gray-400'>About</a>
-          <a href="#Header" className='cursor-pointer hover:text-gray-400'>Projects</a>
-          <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+        <ul className="hidden md:flex gap-7 text-white">
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Home
+          </a>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            About
+          </a>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Projects
+          </a>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Testimonials
+          </a>
         </ul>
-        <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign up</button>
+        <button className="hidden md:block bg-white px-8 py-2 rounded-full">
+          Sign up
+        </button>
+        <img
+          onClick={() => {
+            setShowMobilMenu(true);
+          }}
+          src={assets.menu_icon}
+          className="md:hidden w-7 cursor-pointer"
+          alt=""
+        />
+      </div>
+      {/* mobile menu */}
+      <div
+        className={`md:hidden ${
+          showMobilMenu ? "fixed w-full" : "h-0 w-0 "
+        } right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}
+      >
+        <div className="flex justify-end p-6 cursor-pointer">
+          <img
+            onClick={() => {
+              setShowMobilMenu(false);
+            }}
+            src={assets.cross_icon}
+            className="w-6"
+            alt=""
+          />
+        </div>
+        <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+          <a
+            onClick={() => {
+              setShowMobilMenu(false);
+            }}
+            href="#Home"
+            className="px-4 py-2 rounded-full inline-block"
+          >
+            Home
+          </a>
+          <a
+            onClick={() => {
+              setShowMobilMenu(false);
+            }}
+            href="#About"
+            className="px-4 py-2 rounded-full inline-block"
+          >
+            About
+          </a>
+          <a
+            onClick={() => {
+              setShowMobilMenu(false);
+            }}
+            href="#Projects"
+            className="px-4 py-2 rounded-full inline-block"
+          >
+            Projects
+          </a>
+          <a
+            onClick={() => {
+              setShowMobilMenu(false);
+            }}
+            href="#Testimonials"
+            className="px-4 py-2 rounded-full inline-block"
+          >
+            Testimonials
+          </a>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navebar
+export default Navebar;
